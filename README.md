e28a37a5f98b3faa955f9c6493ec1fa8d562171e
jasonexus <jasonexus7458@gmail.com>
2022-01-28 09:43:34 -0700

    Add files

--- README.md
+++ README.md
@@ -20,6 +20,10 @@ server.py contains contributions from:
 * Jackson Z Chang
 * Mandy Meindersma 
 
+I collaborated on this assignment with:
+
+CCID: junyan4
+
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os
+import os.path
+from pathlib import Path
+import mimetypes
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2022 Abram Hindle, Eddie Antonio Santos, Jason Branch-Allen
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,13 +30,121 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+p = Path('/')
+cwd = os.getcwd()
+# firstPath = cwd / 'www' / 'index.html'
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        data = self.data.decode()
+        http_header = data.split('\r\n')[0]
+        file_path = http_header.split()[1]
+        request_method = http_header.split()[0]
+        extension = file_path.split('/')[-1]
+        path_ending = file_path[-1]
+        secondPath = './www' + file_path
+        print(extension)
+        # print(data)
+        # print(http_header)
+        # print(http_command)
+
+        # Handle HTTP requests
+        if len(data) > 0:
+            if request_method != 'GET':
+                self.request.sendall(bytearray(
+                    "HTTP/1.1 405 Method Not Allowed\r\n", 'utf-8'))
+            # elif path_ending != '/' and (extension.isalpha() or '-' in extension):
+            #     self.request.sendall(
+            #         bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found", 'utf-8'))
+            # elif path_ending != '/' and '.' not in extension:
+            #     # self.request.sendall(
+            #     #     bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found", 'utf-8'))
+            #     self.request.sendall(
+            #         bytearray("HTTP/1.1 301 Moved Permanently\r\n" + file_path + "/" + "\r\n", 'utf-8'))
+            else:
+                self.server_requests(file_path)
+        # else:
+        #     send.request.sendall(
+        #         bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found"))
+
+        print("Got a request of: %s\n" % self.data)
+        self.request.sendall(bytearray("OK\r\n", 'utf-8'))
+
+    def server_requests(self, file_path):
+        firstPath = './www' + file_path
+        secondPath = './www' + file_path + '/index.html'
+        path_ending = file_path[-1]
+        extension = file_path.split('/')[-1]
+
+        try:
+            if os.path.realpath(cwd + '/www' + file_path).startswith(cwd + '/www'):
+                if os.path.exists(firstPath) and extension.isalpha():
+                    # fin = open(firstPath)
+                    # content = fin.read()
+
+                    if os.path.isdir(firstPath):
+                        self.request.sendall(
+                            bytearray("HTTP/1.1 301 Moved Permanently\r\n: " + file_path + '/' + "\r\n", 'utf-8'))
+                    else:
+                        self.request.sendall(
+                            bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found", 'utf-8'))
+                    fin.close()
+                elif os.path.exists(secondPath) and file_path.endswith('/'):
+                    fin = open(secondPath)
+                    content = fin.read()
+                    # print(content)
+                    if content != None:
+                        self.request.sendall(bytearray(
+                            "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + content, 'utf-8'))
+                    else:
+                        self.request.sendall(
+                            bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found", 'utf-8'))
+                    fin.close()
+                elif os.path.exists(firstPath) and (file_path.endswith('.html') or file_path.endswith('.css')):
+                    mimetype, _ = mimetypes.guess_type(file_path)
+                    fin = open("./www" + file_path)
+                    content = fin.read()
+                    # Check if the mimetype is either text/html or text/css
+                    if mimetype == 'text/html':
+                        self.request.sendall(bytearray(
+                            "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + content, 'utf-8'))
+                    elif mimetype == 'text/css':
+                        self.request.sendall(bytearray(
+                            "HTTP/1.1 200 OK\r\n" + "Content-Type: text/css\r\n" + content, 'utf-8'))
+                    fin.close()
+                else:
+                    try:
+                        fin = open(secondPath)
+                        content = fin.read()
+                        self.request.sendall(
+                            bytearray("HTTP/1.1 301 Moved Permanently\r\n: " + file_path + '/' + "\r\n", 'utf-8'))
+                        fin.close()
+                    except FileNotFoundError:
+                        self.request.sendall(
+                            bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found", 'utf-8'))
+
+        except FileNotFoundError:
+            self.request.sendall(
+                bytearray("HTTP/1.1 404 NOT FOUND\r\nFile Not Found", 'utf-8'))
+
+            # elif os.path.exists(firstPath):
+            #     file_path = '/index.html'
+            #     fin = open("./www" + file_path)
+            #     content = fin.read()
+            #     print(content)
+            #     self.request.sendall(bytearray(
+            #         "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + content, 'utf-8'))
+            #     fin.close()
+
+            # elif os.path.exists(file_path) and file_path.endswith('.css'):
+            #     print("Hi")
+            # Check if the path ends with .html. If so, we open it.
+
+            # Check if the
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -40,6 +152,7 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
+    print("Server running on port %s" % PORT)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
